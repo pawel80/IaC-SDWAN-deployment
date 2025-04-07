f2c5581189993bb07b0eb1e2cc08e2addd7a0072
Pawel <pawel80@gmail.com>
2025-04-07 21:48:18 +0200

    templates_v05

--- tf_sdwan/infra.tf
+++ tf_sdwan/infra.tf
@@ -67,3 +67,47 @@ resource "sdwan_transport_wan_vpn_interface_ethernet_feature" "transport_01_wan_
     }
   ]
 }
+
+# CONFIGURATION GROUP:
+resource "sdwan_configuration_group" "config_group_01" {
+  name        = "config_group_01"
+  description = "My config group"
+  solution     = "sdwan"
+  feature_profile_ids = [sdwan_system_feature_profile.system_01.id, sdwan_transport_feature_profile.transport_01.id]
+  # devices = [{
+  #   id     = "C8K-40C0CCFD-9EA8-2B2E-E73B-32C5924EC79B"
+  #   deploy = true
+  #   variables = [
+  #     {
+  #       name = "host_name"
+  #       value = "edge1"
+  #     },
+  #     {
+  #       name = "pseudo_commit_timer"
+  #       value = 0
+  #     },
+  #     {
+  #       name = "site_id"
+  #       value = 1
+  #     },
+  #     {
+  #       name = "system_ip"
+  #       value = "10.1.1.1"
+  #     },
+  #     {
+  #       name = "ipv6_strict_control"
+  #       value = "false"
+  #     }
+  #   ]
+  # }]
+  feature_versions = [
+    sdwan_system_basic_feature.system_01_basic.version,
+    sdwan_system_aaa_feature.system_01_aaa.version,
+    sdwan_system_bfd_feature.system_01_bfd.version,
+    sdwan_system_global_feature.system_01_global.version,
+    sdwan_system_logging_feature.system_01_logging.version,
+    sdwan_system_omp_feature.system_01_omp.version,
+    sdwan_transport_wan_vpn_feature.transport_01_wan_vpn.version,
+    sdwan_transport_wan_vpn_interface_ethernet_feature.transport_01_wan_vpn_interface_ethernet.version,
+  ]
+}
