0b95e243b12a4fbf292325dc79ef84fb574f513a
Pawel <pawel80@gmail.com>
2025-04-12 18:01:25 +0200

    templates_v07_main

--- tf_sdwan/infra.tf
+++ tf_sdwan/infra.tf
@@ -60,7 +60,7 @@ resource "sdwan_transport_wan_vpn_feature" "transport_wan_vpn_v01" {
   name               = "TRANSPORT_WAN_VPN0_v01"
   feature_profile_id = sdwan_transport_feature_profile.transport_v01.id
   vpn                = 0
-  # primary_dns_address_ipv4   = "8.8.8.8"
+  primary_dns_address_ipv4   = "8.8.8.8"
   secondary_dns_address_ipv4 = "1.1.1.1"
   ipv4_static_routes = [
     {
@@ -133,8 +133,8 @@ resource "sdwan_configuration_group" "config_group_v01" {
         value = "false"
       },
       {
-        name = "primary_dns_address_ipv4"
-        value = "4.4.4.4"
+        name = "primary_dns_address_ipv6"
+        value = "2001:0:0:1::0"
       }
       ]
     } 
