64a64b1e5d57ff6693144bef2092083758f447e0
Pawel <pawel80@gmail.com>
2025-04-22 21:01:48 +0200

    legacy_routers_v2

--- tf_iosxe/providers.tf
+++ tf_iosxe/providers.tf
@@ -8,22 +8,16 @@ terraform {
   }
 }
 
-provider "iosxe" {
-  alias    = "ROUTER7"
-  username = "${secrets.LEGACY_USERNAME}"
-  password = "${secrets.LEGACY_PASSWORD}"
-  url      = "https://172.16.10.34"
-}
+# provider "iosxe" {
+#   alias    = "ROUTER7"
+#   username = "${secrets.LEGACY_USERNAME}"
+#   password = "${secrets.LEGACY_PASSWORD}"
+#   url      = "https://172.16.10.34"
+# }
 
-provider "iosxe" {
-  alias    = "ROUTER8"
-  username = "${secrets.LEGACY_USERNAME}"
-  password = "${secrets.LEGACY_PASSWORD}"
-  url      = "https://172.16.10.38"
-}
-
-# provider "sdwan" {
-#   username = "${secrets.SDWAN_MANAGER_USERNAME}"
-#   password = "${secrets.SDWAN_MANAGER_PASSWORD}"
-#   url      = "https://10.12.0.20"
+# provider "iosxe" {
+#   alias    = "ROUTER8"
+#   username = "${secrets.LEGACY_USERNAME}"
+#   password = "${secrets.LEGACY_PASSWORD}"
+#   url      = "https://172.16.10.38"
 # }
