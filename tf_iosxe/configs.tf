53aa9041afa0124fb55c3b03a8cbb4ffd3af35ac
Pawel <pawel80@gmail.com>
2025-04-24 22:44:56 +0200

    iosxe_legacy_cfg_v01

---
+++ tf_iosxe/configs.tf
@@ -0,0 +1,33 @@
+# resource "iosxe_interface_ethernet" "gig1" {
+#   provider                       = iosxe.ROUTER7
+#   type                           = "GigabitEthernet"
+#   name                           = "2"
+#   description                    = "INTRANET"
+#   ipv4_address                   = "172.16.10.34"
+#   ipv4_address_mask              = "255.255.255.252"
+#   shutdown                       = false
+# }
+
+resource "iosxe_interface_ethernet" "gig2" {
+  provider                       = iosxe.ROUTER7
+  type                           = "GigabitEthernet"
+  name                           = "2"
+  description                    = "NOT-USED"
+  shutdown                       = true
+}
+
+resource "iosxe_interface_ethernet" "gig3" {
+  provider                       = iosxe.ROUTER7
+  type                           = "GigabitEthernet"
+  name                           = "3"
+  description                    = "NOT-USED"
+  shutdown                       = true
+}
+
+resource "iosxe_interface_ethernet" "gig4" {
+  provider                       = iosxe.ROUTER7
+  type                           = "GigabitEthernet"
+  name                           = "3"
+  description                    = "NOT-USED"
+  shutdown                       = true
+}
