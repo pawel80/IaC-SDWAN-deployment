7a2462a988e3530212f1e6d4ecbefec70f062bc9
Pawel <pawel80@gmail.com>
2025-04-24 22:58:53 +0200

    iosxe_legacy_cfg_v02

--- tf_iosxe/devices.tf
+++ tf_iosxe/devices.tf
@@ -1,13 +1,13 @@
 provider "iosxe" {
   alias    = "ROUTER7"
-  username = "${secrets.LEGACY_USERNAME}"
-  password = "${secrets.LEGACY_PASSWORD}"
+  username = var.LEGACY_USERNAME
+  password = var.LEGACY_PASSWORD
   url      = "https://172.16.10.34"
 }
 
-provider "iosxe" {
-  alias    = "ROUTER8"
-  username = "${secrets.LEGACY_USERNAME}"
-  password = "${secrets.LEGACY_PASSWORD}"
-  url      = "https://172.16.10.38"
-}
+# provider "iosxe" {
+#   alias    = "ROUTER8"
+#   username = "${secrets.LEGACY_USERNAME}"
+#   password = "${secrets.LEGACY_PASSWORD}"
+#   url      = "https://172.16.10.38"
+# }
