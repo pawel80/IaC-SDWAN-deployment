ab84c715d1870a719f18bb876cbfd8b6d47f6fde
Pawel <pawel80@gmail.com>
2025-04-24 23:03:01 +0200

    iosxe_legacy_cfg_v03

--- tf_iosxe/configs.tf
+++ tf_iosxe/configs.tf
@@ -8,26 +8,34 @@
 #   shutdown                       = false
 # }
 
-resource "iosxe_interface_ethernet" "gig2" {
-  provider                       = iosxe.ROUTER7
-  type                           = "GigabitEthernet"
-  name                           = "2"
-  description                    = "NOT-USED"
-  shutdown                       = true
+resource "iosxe_restconf" "RTR7" {
+  provider   = iosxe.ROUTER7
+  path       = "openconfig-system:system/config"
+  attributes = {
+    hostname = "RTR7"
+  }
 }
 
-resource "iosxe_interface_ethernet" "gig3" {
-  provider                       = iosxe.ROUTER7
-  type                           = "GigabitEthernet"
-  name                           = "3"
-  description                    = "NOT-USED"
-  shutdown                       = true
-}
+# resource "iosxe_interface_ethernet" "gig2" {
+#   provider                       = iosxe.ROUTER7
+#   type                           = "GigabitEthernet"
+#   name                           = "2"
+#   description                    = "NOT-USED"
+#   shutdown                       = true
+# }
 
-resource "iosxe_interface_ethernet" "gig4" {
-  provider                       = iosxe.ROUTER7
-  type                           = "GigabitEthernet"
-  name                           = "3"
-  description                    = "NOT-USED"
-  shutdown                       = true
-}
+# resource "iosxe_interface_ethernet" "gig3" {
+#   provider                       = iosxe.ROUTER7
+#   type                           = "GigabitEthernet"
+#   name                           = "3"
+#   description                    = "NOT-USED"
+#   shutdown                       = true
+# }
+
+# resource "iosxe_interface_ethernet" "gig4" {
+#   provider                       = iosxe.ROUTER7
+#   type                           = "GigabitEthernet"
+#   name                           = "3"
+#   description                    = "NOT-USED"
+#   shutdown                       = true
+# }
