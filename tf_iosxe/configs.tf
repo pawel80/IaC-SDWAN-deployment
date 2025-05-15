a37f086e098f21b4736410557400b1b108e19904
Pawel <pawel80@gmail.com>
2025-05-15 21:43:14 +0200

    ios_xe_config_v9

--- tf_iosxe/configs.tf
+++ tf_iosxe/configs.tf
@@ -1,19 +1,19 @@
-# provider "iosxe" {
-#   alias    = "ROUTER7"
+provider "iosxe" {
+  alias    = "ROUTER7"
 #   # username = var.LEGACY_USERNAME
 #   # password = var.LEGACY_PASSWORD
 #   username = "admin"
 #   password = "Cisco123"
 #   url      = "https://172.16.10.34"
-# }
+}
 
-# resource "iosxe_restconf" "ROUTER7" {
-#   provider   = iosxe.ROUTER7
-#   path       = "openconfig-system:system/config"
-#   attributes = {
-#     hostname = "RTR7"
-#   }
-# }
+resource "iosxe_restconf" "ROUTER7" {
+  provider   = iosxe.ROUTER7
+  path       = "openconfig-system:system/config"
+  attributes = {
+    hostname = "RTR7"
+  }
+}
 
 resource "iosxe_cli" "ROUTER7" {
   cli = <<-EOT
