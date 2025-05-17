13bbacd1de180837263058dd6157ed4ec3535d1c
Pawel <pawel80@gmail.com>
2025-05-17 20:12:35 +0200

    ios_xe_config_v41

--- tf_iosxe/configs.tf
+++ tf_iosxe/configs.tf
@@ -4,21 +4,8 @@ resource "iosxe_system" "system_all" {
   device                      = each.value.name
   hostname                    = each.value.hostname
   # provider                    = iosxe.RT7
-  # ip_bgp_community_new_format = true
-  # ipv6_unicast_routing        = true
-  # ip_source_route             = false
   ip_domain_lookup            = false
   ip_domain_name              = "lab.com"
-  # login_delay                 = 10
-  # login_on_failure            = true
-  # login_on_failure_log        = true
-  # login_on_success            = true
-  # login_on_success_log        = true
-  # multicast_routing_vrfs = [
-  #   {
-  #     vrf = "VRF1"
-  #   }
-  # ]
 }
 
 resource "iosxe_cli" "global_loop123" {
@@ -30,9 +17,6 @@ resource "iosxe_cli" "global_loop123" {
   EOT
 }
 
-# resource "iosxe_save_config" "ROUTER7" {
-# }
-
 resource "iosxe_interface_ethernet" "gig1" {
   for_each                       = {for index,router in local.legacy_routers : router.name => router}
   device                         = each.value.name
@@ -42,6 +26,11 @@ resource "iosxe_interface_ethernet" "gig1" {
   ipv4_address                   = each.value.ip_address
   ipv4_address_mask              = each.value.mask
   shutdown                       = false
+}
+
+resource "iosxe_save_config" "save_cfg" {
+  for_each                       = {for index,router in local.legacy_routers : router.name => router}
+  device                         = each.value.name
 }
 
 # resource "iosxe_interface_ethernet" "gig2" {
