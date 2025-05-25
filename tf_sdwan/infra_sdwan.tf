cd51e91f6c9dde20983d6935c752a794b78c52ac
Pawel <pawel80@gmail.com>
2025-05-25 18:01:41 +0200

    sd-wan_cores_v12

--- tf_sdwan/infra_sdwan.tf
+++ tf_sdwan/infra_sdwan.tf
@@ -157,6 +157,18 @@ resource "sdwan_service_lan_vpn_feature" "vpn511_v01" {
   ]
 }
 
+resource "sdwan_service_lan_vpn_interface_ethernet_feature" "example" {
+  name                       = "Example"
+  # description                = "My Example"
+  feature_profile_id         = "f6dd22c8-0b4f-496c-9a0b-6813d1f8b8ac"
+  # service_lan_vpn_feature_id = "140331f6-5418-4755-a059-13c77eb96037"
+  # shutdown                   = false
+  # interface_name             = "GigabitEthernet3"
+  # interface_description      = "LAN"
+  # ipv4_address               = "1.2.3.4"
+  # ipv4_subnet_mask           = "0.0.0.0"
+}
+
 resource "sdwan_service_lan_vpn_interface_ethernet_feature" "vpn511_gig2_v01" {
   name                       = "VPN511_Gig2_v01"
   # description                = "Legacy DC core routers mgmt int"
