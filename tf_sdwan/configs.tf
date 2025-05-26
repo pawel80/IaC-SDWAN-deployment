19b1d137d46da1d9fa62a85c2f3c4aa3063d774f
Pawel <pawel80@gmail.com>
2025-05-26 19:49:37 +0200

    sd-wan_cores_v27

--- tf_sdwan/configs.tf
+++ tf_sdwan/configs.tf
@@ -155,7 +155,7 @@ resource "sdwan_service_lan_vpn_interface_ethernet_feature" "vpn511_gig2_511_v01
   ipv4_subnet_mask           = "255.255.255.252"
   ipv4_nat                   = false
   ipv4_nat_type              = "pool"
-  # ip_mtu                     = 1496
+  ip_mtu                     = 1490
 }
 
 ################################ Configuration group ##############################
