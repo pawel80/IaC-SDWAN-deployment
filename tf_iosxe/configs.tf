594cc3313d5f3ac32fcfb5300d7e3f619c0d0e04
Pawel <pawel80@gmail.com>
2025-05-27 20:35:41 +0200

    legacy_cores_v14

--- tf_iosxe/configs.tf
+++ tf_iosxe/configs.tf
@@ -2,16 +2,16 @@
 ################################### Legacy EDGEs ##################################
 ###################################################################################
 
-# resource "iosxe_system" "system_all" {
-#   # Looping through the list of objects
-#   # for_each                    = {for index,router in local.legacy_routers : router.name => router}
-#   for_each                    = {for router in local.legacy_routers : router.name => router}
-#   device                      = each.value.name
-#   hostname                    = each.value.hostname
-#   # provider                    = iosxe.RT7
-#   ip_domain_lookup            = false
-#   ip_domain_name              = "lab.com"
-# }
+resource "iosxe_system" "system_all" {
+  # Looping through the list of objects
+  # for_each                    = {for index,router in local.legacy_routers : router.name => router}
+  for_each                    = {for router in local.legacy_routers : router.name => router}
+  device                      = each.value.name
+  hostname                    = each.value.hostname
+  # provider                    = iosxe.RT7
+  ip_domain_lookup            = false
+  ip_domain_name              = "lab.com"
+}
 
 resource "iosxe_interface_ethernet" "gig1" {
   # for_each                       = {for index,router in local.legacy_routers : router.name => router}
@@ -61,7 +61,7 @@ resource "iosxe_save_config" "save_cfg" {
 ###################################################################################
 
 resource "iosxe_system" "core_system_all" {
-  provider                    = iosxe.RTDC1R2
+  # provider                    = iosxe.RTDC1R2
   # for_each                    = {for router in local.legacy_routers : router.name => router}
   # device                      = each.value.name
   hostname                    = "DC1R2"
