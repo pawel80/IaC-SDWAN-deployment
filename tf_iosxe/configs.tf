a9c36079f114ef078d3e666245c439c158abe551
Pawel <pawel80@gmail.com>
2025-06-23 20:44:18 +0200

    bgp_update_v05

--- tf_iosxe/configs.tf
+++ tf_iosxe/configs.tf
@@ -161,16 +161,16 @@ resource "iosxe_bgp_neighbor" "core_bgp_neighbor2" {
   shutdown             = false
 }
 
-# resource "iosxe_bgp_neighbor" "core_bgp_neighbor3_400" {
-#   provider             = iosxe.cores
-#   for_each             = {for router in local.legacy_core_routers : router.name => router}
-#   device               = each.value.name
-#   asn                  = each.value.bgp_asn
-#   ip                   = each.value.bgp_nb3_ip_address
-#   remote_as            = each.value.bgp_nb3_400_asn
-#   description          = each.value.bgp_nb3_desc
-#   shutdown             = false
-# }
+resource "iosxe_bgp_neighbor" "core_bgp_neighbor3_400" {
+  provider             = iosxe.cores
+  for_each             = {for router in local.legacy_core_routers : router.name => router}
+  device               = each.value.name
+  asn                  = each.value.bgp_asn
+  ip                   = each.value.bgp_nb3_ip_address
+  remote_as            = each.value.bgp_nb3_400_asn
+  description          = each.value.bgp_nb3_desc
+  shutdown             = false
+}
 
 resource "iosxe_bgp_ipv4_unicast_neighbor" "core_bgp_neighbor1_af" {
   provider                    = iosxe.cores
@@ -189,6 +189,16 @@ resource "iosxe_bgp_ipv4_unicast_neighbor" "core_bgp_neighbor2_af" {
   depends_on                  = [iosxe_bgp_neighbor.core_bgp_neighbor2]
   asn                         = each.value.bgp_asn
   ip                          = each.value.bgp_nb2_ip_address
+  activate                    = true
+}
+
+resource "iosxe_bgp_ipv4_unicast_neighbor" "core_bgp_neighbor3_400_af" {
+  provider                    = iosxe.cores
+  for_each                    = {for router in local.legacy_core_routers : router.name => router}
+  device                      = each.value.name
+  depends_on                  = [iosxe_bgp_neighbor.core_bgp_neighbor3_400]
+  asn                         = each.value.bgp_asn
+  ip                          = each.value.bgp_nb3_ip_address
   activate                    = true
 }
 
