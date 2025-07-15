83ccc64d289d1f0b88b94e4ad84f221e8e3214e9
Pawel <pawel80@gmail.com>
2025-07-15 20:03:12 +0200

    edges_update_v62

--- tf_sdwan/configs.tf
+++ tf_sdwan/configs.tf
@@ -195,7 +195,7 @@ resource "sdwan_configuration_group" "edge_config_group_v01" {
     sdwan_transport_feature_profile.transport_v01.id,
     sdwan_service_feature_profile.edge_service_v01.id,
   ]
-  # devices = local.sd-wan_edges
+  devices = local.sd-wan_edges
   feature_versions = [
     sdwan_system_basic_feature.system_basic_v01.version,
     sdwan_system_aaa_feature.system_aaa_v01.version,
