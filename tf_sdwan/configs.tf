fd74f5cd075f0545d0a54853cbeef733ad3751a6
Pawel <pawel80@gmail.com>
2025-07-16 18:49:29 +0200

    cores_edges_update_v14

--- tf_sdwan/configs.tf
+++ tf_sdwan/configs.tf
@@ -178,30 +178,30 @@ resource "sdwan_service_lan_vpn_interface_ethernet_feature" "edge_loop_54_v01" {
 # }
 
 ################################ Configuration group ##############################
-# resource "sdwan_configuration_group" "edge_config_group_v01" {
-#   name        = "CG_EDGES_v01"
-#   description = "Configuration group - Edges"
-#   solution     = "sdwan"
-#   feature_profile_ids = [
-#     sdwan_system_feature_profile.edge_system_v01.id, 
-#     sdwan_transport_feature_profile.transport_v01.id,
-#     sdwan_service_feature_profile.edge_service_v01.id,
-#   ]
-#   # devices = local.sd-wan_edges
-#   feature_versions = [
-#     sdwan_system_basic_feature.system_basic_v01.version,
-#     sdwan_system_aaa_feature.system_aaa_v01.version,
-#     # sdwan_system_bfd_feature.system_bfd_v01.version,
-#     # sdwan_system_logging_feature.system_logging_v01.version,
-#     sdwan_system_global_feature.system_global_v01.version,
-#     sdwan_system_omp_feature.edge_system_omp_v01.version,
-#     sdwan_transport_wan_vpn_feature.transport_wan_vpn_v01.version,
-#     sdwan_transport_wan_vpn_interface_ethernet_feature.transport_wan_vpn_if_eth_v01.version,
-#     sdwan_service_lan_vpn_feature.edge_vpn504_v01.version,
-#     sdwan_service_lan_vpn_feature.edge_vpn400_v01.version,
-#     # sdwan_service_lan_vpn_interface_ethernet_feature.edge_loop_54_v01.version,
-#   ]
-# }
+resource "sdwan_configuration_group" "edge_config_group_v01" {
+  name        = "CG_EDGES_v01"
+  description = "Configuration group - Edges"
+  solution     = "sdwan"
+  feature_profile_ids = [
+    sdwan_system_feature_profile.edge_system_v01.id, 
+    sdwan_transport_feature_profile.transport_v01.id,
+    sdwan_service_feature_profile.edge_service_v01.id,
+  ]
+  # devices = local.sd-wan_edges
+  feature_versions = [
+    sdwan_system_basic_feature.system_basic_v01.version,
+    sdwan_system_aaa_feature.system_aaa_v01.version,
+    # sdwan_system_bfd_feature.system_bfd_v01.version,
+    # sdwan_system_logging_feature.system_logging_v01.version,
+    sdwan_system_global_feature.system_global_v01.version,
+    sdwan_system_omp_feature.edge_system_omp_v01.version,
+    sdwan_transport_wan_vpn_feature.transport_wan_vpn_v01.version,
+    sdwan_transport_wan_vpn_interface_ethernet_feature.transport_wan_vpn_if_eth_v01.version,
+    sdwan_service_lan_vpn_feature.edge_vpn504_v01.version,
+    sdwan_service_lan_vpn_feature.edge_vpn400_v01.version,
+    # sdwan_service_lan_vpn_interface_ethernet_feature.edge_loop_54_v01.version,
+  ]
+}
 
 
 
