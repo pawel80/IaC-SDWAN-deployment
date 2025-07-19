803589f8596c48daf13766c99e265a5060f98880
Pawel <pawel80@gmail.com>
2025-07-19 17:41:26 +0200

    cores_edges_update_v43

--- tf_sdwan/configs.tf
+++ tf_sdwan/configs.tf
@@ -112,17 +112,17 @@ resource "sdwan_transport_wan_vpn_interface_ethernet_feature" "edge_dual1_vpn0_i
   tloc_extension               = "GigabitEthernet1"
 }
 
-# resource "sdwan_transport_wan_vpn_interface_ethernet_feature" "edge_dual1_vpn0_if_eth2_v01" {
-#   name                         = "WAN_VPN0_IF_ETH_v02"
-#   feature_profile_id           = sdwan_transport_feature_profile.transport_v01.id
-#   transport_wan_vpn_feature_id = sdwan_transport_wan_vpn_feature.transport_wan_vpn_v01.id
-#   interface_name               = "GigabitEthernet2"
-#   shutdown                     = true
-#   interface_description        = "NOT-USED"
-#   ipv4_configuration_type      = "static"
-#   ipv4_address                 = "1.1.1.1"
-#   ipv4_subnet_mask             = "255.255.255.252"
-# }
+resource "sdwan_transport_wan_vpn_interface_ethernet_feature" "edge_dual1_vpn0_if_eth2_v01" {
+  name                         = "WAN_VPN0_IF_ETH_v02"
+  feature_profile_id           = sdwan_transport_feature_profile.transport_v01.id
+  transport_wan_vpn_feature_id = sdwan_transport_wan_vpn_feature.transport_wan_vpn_v01.id
+  interface_name               = "GigabitEthernet2"
+  shutdown                     = true
+  interface_description        = "NOT-USED"
+  ipv4_configuration_type      = "static"
+  ipv4_address                 = "1.1.1.1"
+  ipv4_subnet_mask             = "255.255.255.252"
+}
 
 # resource "sdwan_transport_wan_vpn_interface_ethernet_feature" "edge_dual1_vpn0_if_eth4_v01" {
 #   name                         = "WAN_VPN0_IF_ETH_v04"
@@ -133,19 +133,19 @@ resource "sdwan_transport_wan_vpn_interface_ethernet_feature" "edge_dual1_vpn0_i
 #   interface_description        = "NOT-USED"
 # }
 
-# resource "sdwan_service_lan_vpn_interface_ethernet_feature" "edge_dual1_vpn400_if_eth2_v01" {
-#   name                       = "EDGE_DUAL1_ETH2_v01"
-#   description                = "NOT-USED"
-#   feature_profile_id         = sdwan_service_feature_profile.edge_service_v01.id
-#   service_lan_vpn_feature_id = sdwan_service_lan_vpn_feature.edge_vpn400_v01.id
-#   interface_name             = "GigabitEthernet2"
-#   shutdown                   = true
-#   interface_description      = "NOT-USED"
-#   ipv4_address               = "1.1.1.1"
-#   ipv4_subnet_mask           = "255.255.255.252"
-#   ipv4_nat                   = false
-#   ipv4_nat_type              = "pool"
-# }
+resource "sdwan_service_lan_vpn_interface_ethernet_feature" "edge_dual1_vpn400_if_eth2_v01" {
+  name                       = "EDGE_DUAL1_ETH2_v01"
+  description                = "NOT-USED"
+  feature_profile_id         = sdwan_service_feature_profile.edge_service_v01.id
+  service_lan_vpn_feature_id = sdwan_service_lan_vpn_feature.edge_vpn400_v01.id
+  interface_name             = "GigabitEthernet2"
+  shutdown                   = true
+  interface_description      = "NOT-USED"
+  ipv4_address               = "1.1.1.1"
+  ipv4_subnet_mask           = "255.255.255.252"
+  ipv4_nat                   = false
+  ipv4_nat_type              = "pool"
+}
 
 resource "sdwan_service_lan_vpn_feature" "edge_vpn504_v01" {
   name                       = "EDGE_VPN504_v01"
