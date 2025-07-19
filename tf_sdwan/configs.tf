ea9e6422369ff1b68fe4ce09c983fb7cc0cf757a
Pawel <pawel80@gmail.com>
2025-07-19 17:50:19 +0200

    cores_edges_update_v45

--- tf_sdwan/configs.tf
+++ tf_sdwan/configs.tf
@@ -430,7 +430,7 @@ resource "sdwan_service_lan_vpn_interface_ethernet_feature" "core_loop_54_v01" {
   service_lan_vpn_feature_id = sdwan_service_lan_vpn_feature.core_vpn504_v01.id
   shutdown                   = false
   interface_name             = "Loopback54"
-  interface_description      = "Monitoring"
+  interface_description      = "Monitoring(open)"
   ipv4_address_variable      = "{{var_core_loop54_address}}"
   ipv4_subnet_mask_variable  = "{{var_core_loop54_mask}}"
   ipv4_nat                   = false
