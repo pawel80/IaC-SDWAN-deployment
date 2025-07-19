7d5a17ac6f864b31091cb80cba9af67f4ac53c8d
Pawel <pawel80@gmail.com>
2025-07-19 19:59:58 +0200

    cores_edges_update_v61

--- tf_sdwan/configs.tf
+++ tf_sdwan/configs.tf
@@ -187,7 +187,7 @@ resource "sdwan_configuration_group" "edge_config_group_v01" {
     sdwan_transport_feature_profile.edge_transport_v01.id,
     sdwan_service_feature_profile.edge_service_v01.id,
   ]
-  # devices = local.sdwan_edges_dual1
+  devices = local.sdwan_edges_dual1
   feature_versions = [
     sdwan_system_basic_feature.system_basic_v01.version,
     sdwan_system_aaa_feature.system_aaa_v01.version,
@@ -736,7 +736,7 @@ resource "sdwan_configuration_group" "core_config_group_v01" {
     sdwan_service_feature_profile.core_service_v01.id, 
     sdwan_cli_feature_profile.core_cli_v01.id,
   ]
-  devices = local.sdwan_cores
+  # devices = local.sdwan_cores
   feature_versions = [
     sdwan_system_basic_feature.system_basic_v01.version,
     sdwan_system_aaa_feature.system_aaa_v01.version,
