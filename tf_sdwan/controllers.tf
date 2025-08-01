d97e87ed7d456ca9daf29febc7a05df025e3cda2
Pawel <pawel80@gmail.com>
2025-08-01 17:52:32 +0200

    policy_update_v43

--- tf_sdwan/controllers.tf
+++ tf_sdwan/controllers.tf
@@ -100,18 +100,7 @@ vpn 512
 
 resource "sdwan_attach_feature_device_template" "controller_attached_v1" {
   id = sdwan_cli_device_template.controller_cli_v1.id
-  devices = local.controllers
-  # devices = [
-  #   {
-  #     id = "C8K-CC678D1C-8EDF-3966-4F51-ABFAB64F5ABE"
-  #     variables = {
-  #       system_site_id                                  = "1001"
-  #       system_system_ip                                = "1.1.1.1"
-  #       system_host_name                                = "router1"
-  #       vpn_if_name_Default_vEdge_DHCP_Tunnel_Interface = "GigabitEthernet1"
-  #     }
-  #   }
-  # ]
+  # devices = local.controllers
 }
 
 
