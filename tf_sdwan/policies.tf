2c4c744bdcf590bb814a3a72fad80e829c649bfc
Pawel <pawel80@gmail.com>
2025-08-02 19:58:43 +0200

    policy_update_v73

--- tf_sdwan/policies.tf
+++ tf_sdwan/policies.tf
@@ -107,13 +107,25 @@ resource "sdwan_hub_and_spoke_topology_policy_definition" "hub_spoke_v1" {
   ]
 }
 
-resource "sdwan_custom_control_topology_policy_definition" "hub_filter_traffic_v1" {
-  name           = "HUB-FILTER-TRAFFIC_v1"
-  description    = "Filter traffic from HUB to SPOKES"
+resource "sdwan_custom_control_topology_policy_definition" "hub_spoke_filter_traffic_v1" {
+  name           = "HUB-SPOKE_FILTER-TRAFFIC_v1"
+  description    = "Force HUB to SPOKES plus traffic filtering"
   default_action = "reject"
   sequences = [
     {
-      id          = 1
+      id          = 10
+      name        = "HUB-TLOCs"
+      type        = "tloc"
+      base_action = "accept"
+      match_entries = [
+        {
+          type    = "siteList"
+          site_list_id  = sdwan_site_list_policy_object.hubs_v1.id
+        }
+      ]
+    },
+    {
+      id          = 20
       name        = "Default_route"
       type        = "route"
       ip_type     = "ipv4"
@@ -130,22 +142,29 @@ resource "sdwan_custom_control_topology_policy_definition" "hub_filter_traffic_v
 
 resource "sdwan_centralized_policy" "hub_spoke_policy_v1" {
   name        = "POLICY-HUB-SPOKE_v1"
-  description = "Centralized Policy: HUB-SPOKE"
+  description = "Centralized Policy: HUB-SPOKE and traffic filtering"
   definitions = [
+    # {
+    #   id   = sdwan_hub_and_spoke_topology_policy_definition.hub_spoke_v1.id
+    #   type = "hubAndSpoke"
+    #   # entries = [
+    #   #   {
+    #   #     site_list_ids = [sdwan_site_list_policy_object.hubs_v1.id, sdwan_site_list_policy_object.spokes_v1.id]
+    #   #     vpn_list_ids  = [sdwan_vpn_list_policy_object.vpns_v1.id]
+    #   #     direction     = "out"
+    #   #   }
+    #   # ]
+    # },
     {
-      id   = sdwan_hub_and_spoke_topology_policy_definition.hub_spoke_v1.id
-      type = "hubAndSpoke"
-      # entries = [
-      #   {
-      #     site_list_ids = [sdwan_site_list_policy_object.hubs_v1.id, sdwan_site_list_policy_object.spokes_v1.id]
-      #     vpn_list_ids  = [sdwan_vpn_list_policy_object.vpns_v1.id]
-      #     direction     = "out"
-      #   }
-      # ]
-    },
-    {
-      id = sdwan_custom_control_topology_policy_definition.hub_filter_traffic_v1.id
+      id = sdwan_custom_control_topology_policy_definition.hub_spoke_filter_traffic_v1.id
       type = "control"
+      entries = [
+        {
+          site_list_ids = [sdwan_site_list_policy_object.spokes_v1.id]
+          # vpn_list_ids  = [sdwan_vpn_list_policy_object.vpns_v1.id]
+          direction     = "out"
+        }
+      ]
     }
   ]
 }
@@ -155,6 +174,6 @@ resource "sdwan_centralized_policy" "hub_spoke_policy_v1" {
 
 
 #--------------------- Activate/De-activate Centralized Policy --------------------
-resource "sdwan_activate_centralized_policy" "activate_centralized_policy_v1" {
-  id = sdwan_centralized_policy.hub_spoke_policy_v1.id
-}
+# resource "sdwan_activate_centralized_policy" "activate_centralized_policy_v1" {
+#   id = sdwan_centralized_policy.hub_spoke_policy_v1.id
+# }
