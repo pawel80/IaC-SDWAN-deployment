af7e21049ed9ff9107b0c1a59f225a1b9c7cf834
Pawel <pawel80@gmail.com>
2025-08-05 20:13:29 +0200

    ospf_cfg_v05

--- tf_sdwan/configs.tf
+++ tf_sdwan/configs.tf
@@ -1085,58 +1085,29 @@ resource "sdwan_service_routing_ospf_feature" "core_ospf_200_v01" {
   description                               = "OSPF for Legacy routers"
   feature_profile_id                        = sdwan_service_feature_profile.core_service_v01.id
   router_id_variable                        = "{{var_core_loop20_address}}"
-  # reference_bandwidth                       = 101
-  # rfc_1583_compatible                       = true
-  # default_information_originate             = false
-  # default_information_originate_always      = false
-  # default_information_originate_metric      = 1
-  # default_information_originate_metric_type = "type1"
-  # distance_external                         = 110
-  # distance_inter_area                       = 110
-  # distance_intra_area                       = 110
-  # spf_calculation_delay                     = 200
-  # spf_initial_hold_time                     = 1000
-  # spf_maximum_hold_time                     = 10000
-  # redistributes = [
-  #   {
-  #     protocol = "static"
-  #     nat_dia  = true
-  #   }
-  # ]
-  # router_lsas = [
-  #   {
-  #     type = "on-startup"
-  #     time = 5
-  #   }
-  # ]
+  redistributes = [
+    {
+      protocol = "bgp"
+    }
+  ]
   areas = [
     {
       area_number = 0
-      # area_type   = "stub"
-      # no_summary  = false
       interfaces = [
         {
           name                       = "Tunnel15000512"
-          # hello_interval             = 10
-          # dead_interval              = 40
-          # lsa_retransmit_interval    = 5
-          # cost                       = 10
-          # designated_router_priority = 1
           network_type               = "point-to-point"
           passive_interface          = false
-          # authentication_type        = "message-digest"
-          # message_digest_key_id      = 7
-          # message_digest_key         = "sdjfhsghbjdjr"
         }
       ]
-      ranges = [
-        {
-          ip_address   = "10.1.1.0"
-          subnet_mask  = "255.255.255.0"
-          cost         = 1
-          no_advertise = false
-        }
-      ]
+      # ranges = [
+      #   {
+      #     ip_address   = "10.1.1.0"
+      #     subnet_mask  = "255.255.255.0"
+      #     cost         = 1
+      #     no_advertise = false
+      #   }
+      # ]
     }
   ]
 }
